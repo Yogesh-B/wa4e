77505b922c4f7c1d085e69c3327852605e5274f8
Yogesh <41276813+Yogesh-B@users.noreply.github.com>
2020-06-26 18:39:25 +0530

    week4 added

---
+++ week4code/autos.php
@@ -0,0 +1,72 @@
+<?php
+if ( !isset($_GET['name']) )
+        die("Name parameter missing");
+if ( isset($_POST['logout']) )
+        header("location: index.php");
+$failure = false;
+require_once "pdo.php";
+//session_start();
+
+    if ( isset($_POST['add']) ) {
+        
+        if ( strlen($_POST['make']) < 1 )
+        {
+            $failure = "Make is required";
+        }
+        else if ( !is_numeric($_POST['mileage']) || !is_numeric($_POST['year']) ){
+            $failure = "Mileage and year must be numeric";
+        }
+        else {
+            $send_query = $pdo->prepare("INSERT INTO autos ( make, year, mileage ) VALUES (:mk,:yr,:mi)");
+            $send_query->execute(array(
+                ':mk'=>$_POST['make'],
+                ':yr'=>$_POST['year'],
+                ':mi'=>$_POST['mileage']
+                ));
+        }
+        
+   }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Yogesh Bavishi's Automobile Tracker</title>
+        <?php require_once "bootstrap.php"; ?>
+</head>
+    <body>
+        <div class="container">
+        <h1>Tracking Autos for <?php echo $_GET['name'];?></h1>
+        <?php
+        if ( $failure !== false ) {
+            // validation of data
+            echo('<p style="color: red;">'.htmlentities($failure)."</p><br/>");
+        }
+        else if( isset($_POST['add']) ){
+            echo('<p style="color: green;">Record inserted</p><br/>');
+        }
+
+        ?>
+        <form method="POST">
+            <p>Make:
+            <input type="text" name="make" size="60"/></p>
+            <p>Year:
+            <input type="text" name="year"/></p>
+            <p>Mileage:
+            <input type="text" name="mileage"/></p>
+            <input type="submit" name="add" value="Add">
+            <input type="submit" name="logout" value="Logout">
+        </form>
+        <h2>Automobiles</h2>
+        <p>
+            <?php              //list of automobiles from database
+                $fetch_query = $pdo->prepare('SELECT * FROM autos');
+                $fetch_query->execute();
+                while ( $row = $fetch_query->fetch(PDO::FETCH_ASSOC) ) {
+                   echo "<li>".$row['year']."  ".htmlentities($row['make'])."/".$row['mileage']."</li>";
+                }
+            ?>
+        </p>
+        </div>
+    </body>
+</html>
---
+++ week4code/login.php
@@ -0,0 +1,80 @@
+<?php // Do not put any HTML above this line
+
+session_start();
+
+
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';     //pwd is php123
+
+
+// Check to see if we have some POST data, if we do process it
+if ( isset($_POST['who']) && isset($_POST['pass']) ) {
+   
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+        $_SESSION['error'] = "User name and password are required";
+        header("Location: login.php");
+        return;
+    } 
+    else if(!preg_match('/.*@.*/i',$_POST['who'])){
+        $_SESSION['error'] =  "Email must have an at-sign (@)";
+        header("Location: login.php");
+        return;
+        }
+    else {
+        $check = hash('md5', $salt.$_POST['pass']);
+        if ( $check == $stored_hash ) {
+            // Redirect the browser to autos.php
+            error_log("Login success ".$_POST['who']);
+            $_SESSION['name'] = $_POST['who'];
+            $_SESSION['success'] = "Login successfull";
+            header("Location: view.php");
+            return;
+        } 
+        else {
+            error_log("Login fail ".$_POST['who']." $check");
+            $_SESSION['error'] = "Incorrect password";
+            header("Location: login.php");
+            return;
+        }
+    }
+}
+
+// Fall through into the View
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once "bootstrap.php"; ?>
+<title>Yogesh Bavishi's Login Page</title>
+</head>
+<body>
+<div class="container">
+<h1>Please Log In</h1>
+<?php
+// Note triple not equals and think how badly double
+// not equals would work here...
+if ( isset($_SESSION['error']) ) {
+    echo('<p style="color: red;">'.htmlentities($_SESSION['error'])."</p>\n");
+    unset($_SESSION['error']);
+}
+?>
+<form method="POST">
+<label for="nam">User Name</label>
+<input type="text" name="who" id="nam"><br/>
+<label for="id_1723">Password</label>
+<input type="text" name="pass" id="id_1723"><br/>
+<input type="submit" value="Log In">
+<a href="index.php">Cancel</a>
+</form>
+<p>
+For a password hint, view source and find a password hint
+in the HTML comments.
+<!-- Hint:
+The account is csev@umich.edu
+The password is the three character name of the
+programming language used in this class (all lower case)
+followed by 123. -->
+</p>
+</div>
+</body>
+</html>
