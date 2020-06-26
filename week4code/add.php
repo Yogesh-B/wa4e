e036b7c77b0ffc20edb6261701b45d22d1bf8405
Yogesh <41276813+Yogesh-B@users.noreply.github.com>
2020-06-26 19:12:36 +0530

    fixed add.php

--- week4code/add.php
+++ week4code/add.php
@@ -1,18 +1,22 @@
 <?php
 session_start();
-require_once "pdo.php";  
-
 if (!isset($_SESSION['name'])){
     die("Not logged in");
 }
+
+require_once "pdo.php";  
+
+
 if ( isset($_POST['add']) ) {
         
     if ( strlen($_POST['make']) < 1 )
     {
         $_SESSION['error'] = "Make is required";
+        header("Location: add.php");
     }
     else if ( !is_numeric($_POST['mileage']) || !is_numeric($_POST['year']) ){
         $_SESSION['error'] = "Mileage and year must be numeric";
+        header("Location: add.php");
     }
     else {
         $send_query = $pdo->prepare("INSERT INTO autos ( make, year, mileage ) VALUES (:mk,:yr,:mi)");
